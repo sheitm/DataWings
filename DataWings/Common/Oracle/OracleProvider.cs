55b4405171c0a381b91c21f211961b5cd55a33e9
Ståle Heitmann <ssheitmann@gmail.com>
2015-07-09 12:00:42 +0200

    move to .net 4.5

--- DataWings/Common/Oracle/OracleProvider.cs
+++ DataWings/Common/Oracle/OracleProvider.cs
@@ -1,5 +1,5 @@
 ﻿using System.Data.Common;
-using System.Data.OracleClient;
+using Oracle.ManagedDataAccess.Client;
 
 namespace DataWings.Common.Oracle
 {
---
+++ tests/DataWings.IntegrationTests/ConnectionExecutorFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using DataWings.Common;
+using NUnit.Framework;
+
+namespace DataWings.IntegrationTests
+{
+    [TestFixture]
+    public class ConnectionExecutorFinderTests
+    {
+        [Test]
+        [ConnectionFromConfigFile(SqlVendor.Oracle, Key = "CAB", Name = "CAB")]
+        public void GetSqlExecutor()
+        {
+            var provider = ConnectionExecutorFinder.GetSqlExecutor("CAB");
+
+            ////var mi = typeof (ConnectionExecutorFinderTests).GetMethod("GetSqlExecutor",
+            ////    BindingFlags.Instance | BindingFlags.Public);
+            //var mi = typeof(ConnectionExecutorFinderTests).GetMethod("GetSqlExecutor");
+
+            //var attrib = Attribute.GetCustomAttributes(mi, typeof(AbstractConnectionAttribute));
+        }
+    }
+}
